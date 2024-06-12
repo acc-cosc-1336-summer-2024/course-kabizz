870c70cc2d3b1e8e6b68adfe64ce92dc8efe8680
kabizz <zachary.lutz@g.austincc.edu>
2024-06-12 16:27:47 +0000

    Homework 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,15 +1,15 @@
-#Returns the Ratio
+#Returns the Ratio of the score
 def get_options_ratio(option, total):
     return (option / total)
 
-#Returns Faculty Rating
+#Ratings compared to test score
 Excellent = .9
 Very_Good = .8
 Good = .7
 Needs_Improvement = .6
 Unacceptable = 0
 def get_faculty_rating(ratio):
-      
+ #Returns rating compared to test score     
       if ratio >= Excellent:
         return ('Excellent')
       elif ratio >= Very_Good:
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,8 +1,13 @@
-#
 import decisions
-#Get input from user           
-average = decisions.get_options_ratio(int(input('Enter your test score: ')), int(input('Enter total possible points that can be made on test: ')))
+#Get test score from user 
+score = int(input('Enter your test score: ')) 
+#Get total points possible points in test from user
+total = int(input('Enter total possible points that can be made on test: '))  
+#This calculates the score to be displayed for user     
+average = decisions.get_options_ratio(score, total)
+#This calculates the rating to be displayed for user
 rating = decisions.get_faculty_rating (average)
+#This displays the score and the rating to the user
 print ('Your score is', average, 'percent.', 'Which is', rating)
  
 
