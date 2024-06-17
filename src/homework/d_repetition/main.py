89a5d360a29eca75cf6f64d66cf111f823483755
kabizz <zachary.lutz@g.austincc.edu>
2024-06-17 17:49:23 +0000

    Homework 4

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,13 @@
-#
+import repetition
+
+while True:
+    try:
+        num = int(input('1. Enter a number between 1 and 10 to get its factorial: '))
+        average = repetition.get_factorial(num)
+        if num < 1 or num > 10:
+            raise ValueError
+        print(average)
+        break
+    except ValueError:
+        print('Wrong Entry.')
+
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,15 @@
-#
+def get_factorial(num):
+    fact = 1
+    for num in range(1, num + 1):
+        fact *= num
+    return fact
+ 
+def sum_odd_numbers(num1):
+    value = 1
+    total = 0
+    while value < (2*num1) - 1:
+        if value % 2 == 1:
+            total += value
+        value += 1
+    return total
+
